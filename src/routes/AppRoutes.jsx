b7196f2bb9b1c949e43feaedc43e792373d83b1b
akaomarfahimofficial_voidtech23 <akaomarfahimofficial@gmail.com>
2025-06-12 13:23:28 +0600

    home -> Home : AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 // Pages
-import Home from "../pages/home";
+import Home from "../pages/Home";
 import FavoritesScreen from "../pages/favorites";
 import MovieDetailsScreen from "../pages/MovieDetails";
 import ProfileScreen from "../pages/Profile";
